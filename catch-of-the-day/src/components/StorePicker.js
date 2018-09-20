91bd4fc14cccfd24fcc053f98ea6cdf47307afea
Mario <mariob360@gmail.com>
2018-09-20 14:25:50 -0700

    Import react and export StorePicker class

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>I am the StorePicker!</p>
+  }
+}
+
+
+// Must Export as a default
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,13 @@
-// let's go!
+// React Npm Module
+import React from 'react';
+
+// Module for react to interact with the DOM
+// 'react-dom' has several Methods but we only want render
+// We can now render to the dom
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+// 2 Parameters ([JSX - ~HMTL] , [Mounting Point - Actual Dom Element])
+render(<StorePicker/>, document.querySelector('#main'));
+
