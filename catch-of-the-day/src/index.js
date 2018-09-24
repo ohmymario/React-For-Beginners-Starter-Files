7087479109c643afe6ddd6b9d71c7057e61732aa
Mario <mariob360@gmail.com>
2018-09-23 18:09:09 -0700

    Begin routes

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,16 +1,7 @@
-// React Npm Module
 import React from 'react';
-
-// Module for react to interact with the DOM
-// 'react-dom' has several Methods but we only want render
-// We can now render to the dom
 import { render } from 'react-dom';
-
-import StorePicker from './components/StorePicker';
-import App from './components/App';
-
+import Router from './components/Router';
 import "./css/style.css";
 
-// 2 Parameters ([JSX - ~HMTL] , [Mounting Point - Actual Dom Element])
-render(<App/>, document.querySelector('#main'));
+render(<Router/>, document.querySelector('#main'));
 
