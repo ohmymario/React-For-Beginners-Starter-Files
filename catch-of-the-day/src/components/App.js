5eefb6837301d51ca3566ef284b800ff7242bfa2
Mario <mariob360@gmail.com>
2018-10-23 18:38:20 -0700

    Passed down addFish to AddFishForm component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,27 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    // Here you never touch the current state
+    // instead copy then insert new state changes into copy
+    // then replace current state. 
+
+    // 1. Create a copy of the state 
+    const fishes = { ...this.state.fishes }
+    // 2. Add our new fish to the 'fishes' variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({
+      fishes
+    })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +32,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
