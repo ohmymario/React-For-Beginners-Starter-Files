dca09cc1cc727b2e61347c279949cc0362e56719
Mario <mariob360@gmail.com>
2018-10-24 18:29:05 -0700

    Populate fish state with premade fish and display in view

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
 
@@ -10,6 +12,7 @@ class App extends React.Component {
     order: {}
   };
 
+  // Add fish to state
   addFish = (fish) => {
     // Here you never touch the current state
     // instead copy then insert new state changes into copy
@@ -25,14 +28,22 @@ class App extends React.Component {
     })
   }
 
+  // Populate 9 Fishes into State
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
